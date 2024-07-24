c9dd9f33a72b4f075521f8957bebafbcc49534b9
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-07-24 10:48:39 +0200

    Update number.js

--- core/config/number.js
+++ core/config/number.js
@@ -10,7 +10,7 @@ import { fileURLToPath } from "url";
 import { watchFile, unwatchFile } from "fs";
 
 export default {
-    bot: "62xxxxxx",
+    bot: "265997892606",
     owner: ["6289513081052"],
 }
 
